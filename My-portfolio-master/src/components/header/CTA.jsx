f6f86be890c405bd5f7a67eb15d2b971d1f0d75f
SajanKumarSingh <kumarsajan0013@gmail.com>
2025-02-11 22:39:17 +0530

    Resume Updated

--- My-portfolio-master/src/components/header/CTA.jsx
+++ My-portfolio-master/src/components/header/CTA.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import CV from '../../assets/New Resume.pdf';
+import React from "react";
+import CV from "../../assets/New Resume2.pdf";
 
 const CTA = () => {
   return (
-    <div className="cta" >
-      <a style={{marginRight:"10px" ,gap :"10px" ,padding:"10px"}} href={CV} download className="btn">
+    <div className="cta">
+      <a
+        style={{ marginRight: "10px", gap: "10px", padding: "10px" }}
+        href={CV}
+        download
+        className="btn"
+      >
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
